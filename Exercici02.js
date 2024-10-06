86097610c9eaef1ad138f8d229dbb1a177decd80
HodeonArtz <martidicianolold@gmail.com>
2024-10-06 18:44:55 +0200

    #5 botón de pausar implementado

--- Exercici02.js
+++ Exercici02.js
@@ -25,30 +25,62 @@ const outputDisplay = document.querySelector(".output-display"),
 
 let intervalCD;
 let countdownTime = 0;
+let pausedCD = true;
+/**
+ * Permite restar la cuentra atras 1 segundo y actualizar el texto de salida
+ * @returns void
+ */
+function updateCountdown() {
+  if (countdownTime === 0) {
+    clearInterval(intervalCD);
+    intervalCD == null;
+    return;
+  }
+  countdownTime--;
+  outputMinutes.textContent = (countdownTime / 60).toFixed(0).padStart(2, 0);
+  outputSeconds.textContent = (((countdownTime / 60) % 1) * 60)
+    .toFixed(0)
+    .padStart(2, 0);
+}
 
-btnStartCD.addEventListener("click", (event) => {
+function togglePauseBtn() {
+  if (btnPauseCD.textContent === "Pausar") {
+    btnPauseCD.textContent = "Reanudar";
+    return;
+  }
+  btnPauseCD.textContent = "Pausar";
+}
+
+btnStartCD.addEventListener("click", (e) => {
   countdownTime = +inputMinutes.value * 60 + +inputSeconds.value;
-  if (intervalCD) clearInterval(intervalCD);
-  /**
-   * Permite restar la cuentra atras 1 segundo y actualizar el texto de salida
-   * @returns void
-   */
-  function updateCountdown() {
-    if (countdownTime === 0) {
-      clearInterval(intervalCD);
-      return;
-    }
-    countdownTime--;
-    outputMinutes.textContent = (countdownTime / 60).toFixed(0).padStart(2, 0);
-    outputSeconds.textContent = (((countdownTime / 60) % 1) * 60)
-      .toFixed(0)
-      .padStart(2, 0);
+  if (intervalCD) {
+    clearInterval(intervalCD);
+    intervalCD = null;
   }
   updateCountdown();
   intervalCD = setInterval(updateCountdown, 1000);
+  pausedCD = true;
+  btnPauseCD.textContent = "Pausar";
 });
-btnStopCD.addEventListener("click", (event) => {
-  if (intervalCD) clearInterval(intervalCD);
+
+btnStopCD.addEventListener("click", (e) => {
+  if (intervalCD) {
+    clearInterval(intervalCD);
+    intervalCD = null;
+  }
   outputMinutes.textContent = outputSeconds.textContent = "00";
   inputMinutes.value = inputSeconds.value = 0;
+  pausedCD = true;
+  btnPauseCD.textContent = "Pausar";
+});
+
+btnPauseCD.addEventListener("click", (e) => {
+  if (!intervalCD) return;
+  togglePauseBtn();
+  pausedCD = !pausedCD;
+  if (pausedCD) {
+    intervalCD = setInterval(updateCountdown, 1000);
+    return;
+  }
+  clearInterval(intervalCD);
 });
