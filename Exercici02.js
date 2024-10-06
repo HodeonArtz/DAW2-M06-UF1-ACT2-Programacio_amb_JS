2ebcfa45e1612ac0f3928ded0e677c96c1f8f2c9
HodeonArtz <martidicianolold@gmail.com>
2024-10-06 17:51:17 +0200

    #5 botón de iniciar cuenta atrás implementado

--- Exercici02.js
+++ Exercici02.js
@@ -1,23 +1,43 @@
 "use strict";
-const celdas = document.querySelectorAll(
+const infoCells = document.querySelectorAll(
   "#taula_propietats tr > td:nth-child(even)"
 );
-const elValorMaxJS = celdas[0],
-  elAlturaTotalPantalla = celdas[1],
-  elAlturaInternaVentana = celdas[2],
-  elURL = celdas[3];
-elValorMaxJS.textContent = Number.MAX_VALUE;
-elAlturaTotalPantalla.textContent = screen.height;
-elAlturaInternaVentana.textContent = window.innerHeight;
+const elMaxValueJS = infoCells[0],
+  elTotalScreenHeight = infoCells[1],
+  elTotalInnerHeight = infoCells[2],
+  elURL = infoCells[3];
+elMaxValueJS.textContent = Number.MAX_VALUE;
+elTotalScreenHeight.textContent = screen.height;
+elTotalInnerHeight.textContent = window.innerHeight;
 elURL.textContent = window.location.href;
 
 // <<===========||===========||===========||===========>>
 
-const btnCuentaAtras = document.querySelector("button.start-countdown"),
-  inputMinutos = document.querySelector("input.input-minutos"),
-  inputSegundos = document.querySelector("input.input-segundos");
+const btnStartCD = document.querySelector(".btn-start-countdown"),
+  btnStopCD = document.querySelector(".btn-stop-countdown"),
+  btnPauseCD = document.querySelector(".btn-pause-countdown"),
+  inputMinutes = document.querySelector("input.input-minutos"),
+  inputSeconds = document.querySelector("input.input-segundos");
 
-btnCuentaAtras.addEventListener("click", (event) => {
-  console.log(inputMinutos.value);
-  console.log(inputSegundos.value);
+const outputDisplay = document.querySelector(".output-display"),
+  outputMinutes = outputDisplay.querySelector(".minutes-output"),
+  outputSeconds = outputDisplay.querySelector(".seconds-output");
+let intervalCD;
+
+btnStartCD.addEventListener("click", (event) => {
+  let countdownTime = +inputMinutes.value * 60 + +inputSeconds.value;
+  if (intervalCD) clearInterval(intervalCD);
+  function updateCountdown() {
+    if (countdownTime === 0) {
+      clearInterval(intervalCD);
+      return;
+    }
+    countdownTime--;
+    outputMinutes.textContent = (countdownTime / 60).toFixed(0).padStart(2, 0);
+    outputSeconds.textContent = (((countdownTime / 60) % 1) * 60)
+      .toFixed(0)
+      .padStart(2, 0);
+  }
+  updateCountdown();
+  intervalCD = setInterval(updateCountdown, 1000);
 });
